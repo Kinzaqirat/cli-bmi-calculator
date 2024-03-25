656b3a24414c9f7605cc5e6d27b037335671279f
Qiratsumra <saeedqirat43@gmail.com>
2024-03-25 21:52:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your weight in kg", type: "number", name: "weightKg" },
+    { message: "Enter your height in meters", type: "number", name: "heightMeters" },
+]);
+if (answer.weightKg === "weightkg", "heightMeters") {
+    console.log(answer.weightKg / (answer.heightMeters * answer.heightMeters));
+}
+else {
+    console.log("Please enter valid values");
+}
